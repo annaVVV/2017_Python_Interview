1417f239ac1d416d0ab482f6f19c14a33f3be8a9
annaVVV <vergeles.anna@gmail.com>
2017-07-20 16:30:06 -0700

    Update 1_merging_2_sorted_lists.py

--- 1_merging_2_sorted_lists.py
+++ 1_merging_2_sorted_lists.py
@@ -57,3 +57,36 @@ def merge3(a,b):
 Expected result:
 [1, 3, 4, 5, 6, 7, 10, 11, 12, 13, 18, 19, 21]
 '''
+
+# merge 2 sorted lists
+
+def merge(a, b):
+    i, j = 0, 0
+    la = len(a)
+    lb = len(b)
+    ab = []
+    while i < la and j < lb:
+        if a[i] < b[j]:
+            ab.append(a[i])
+            i = i + 1
+        else:
+            ab.append(b[j])
+            j = j + 1
+
+    if i == la:
+        ab.extend(b[j:])
+    if j == lb:
+        ab.extend(a[i:])
+    return ab
+
+
+
+a = [3, 4, 6, 10, 11, 18]
+b = [1, 5, 7, 12, 13, 19, 21]
+
+print a, b
+print merge(a, b)
+
+# Output:
+# [3, 4, 6, 10, 11, 18] [1, 5, 7, 12, 13, 19, 21]
+# [1, 3, 4, 5, 6, 7, 10, 11, 12, 13, 18, 19, 21]
