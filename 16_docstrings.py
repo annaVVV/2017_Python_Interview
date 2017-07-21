790ab77dc401a3c042f58173dc5d55578afbf350
annaVVV <vergeles.anna@gmail.com>
2017-07-21 13:22:39 -0700

    Create 16_docstrings.py

---
+++ 16_docstrings.py
@@ -0,0 +1,21 @@
+"""Docstring Example
+Let's show how an example of a multi-line docstring:
+"""
+
+def my_function():
+    """Do nothing, but document it.
+
+    No, really, it doesn't do anything.
+    """
+    pass
+
+print my_function.__doc__
+
+""" Output:
+Do nothing, but document it.
+
+    No, really, it doesn't do anything.
+Declaration of docstrings
+"""
+
+# Use: http://www.pythonforbeginners.com/basics/python-docstrings
