1eed5f3bc48210af34b992193aa6ed10549e962f
annaVVV <vergeles.anna@gmail.com>
2017-07-24 19:55:40 -0700

    Create File_count_lines_words.py

---
+++ File_count_lines_words.py
@@ -0,0 +1,21 @@
+# Open a file, read line by line, print the number of lines, print the number of words.
+#
+# filename: Test.txt
+# hello
+# blue is my favourite color
+# yellow is also my favourite color
+#
+#
+# output:
+# Number of lines: 3
+# Number of words: 12
+#
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+print 'Number of lines:', file_len("text.txt")
+str = open("text.txt").read()
+print 'Number of words:', len(str.split())
