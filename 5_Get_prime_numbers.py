f608f5e129daedea163ce469c46d3f5cdd5f47b5
annaVVV <vergeles.anna@gmail.com>
2017-07-24 20:16:45 -0700

    Update 5_Get_prime_numbers.py

--- 5_Get_prime_numbers.py
+++ 5_Get_prime_numbers.py
@@ -1,35 +1,4 @@
-# merge 2 sorted lists
-
-def merge(a, b):
-    i, j = 0, 0
-    la = len(a)
-    lb = len(b)
-    ab = []
-    while i < la and j < lb:
-        if a[i] < b[j]:
-            ab.append(a[i])
-            i = i + 1
-        else:
-            ab.append(b[j])
-            j = j + 1
-
-    if i == la:
-        ab.extend(b[j:])
-    if j == lb:
-        ab.extend(a[i:])
-    return ab
-
-
-
-a = [3, 4, 6, 10, 11, 18]
-b = [1, 5, 7, 12, 13, 19, 21]
-print 'Merge sorted lists:'
-print a, b
-print merge(a, b)
-
-# Output:
-# [3, 4, 6, 10, 11, 18] [1, 5, 7, 12, 13, 19, 21]
-# [1, 3, 4, 5, 6, 7, 10, 11, 12, 13, 18, 19, 21]
+# Get prima number from 1 to 100
 
 def primes(n=1):
    while n < 100:
