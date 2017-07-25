005cc39a304081fce95c83ebe80dd3fb9816e3d8
annaVVV <vergeles.anna@gmail.com>
2017-07-24 20:03:28 -0700

    Update File_count_lines_words.py

--- File_count_lines_words.py
+++ File_count_lines_words.py
@@ -19,3 +19,13 @@ def file_len(fname):
 print 'Number of lines:', file_len("text.txt")
 str = open("text.txt").read()
 print 'Number of words:', len(str.split())
+
+# One more:
+lines, words = 0, 0
+with open('text.txt') as file:
+    for line in file:
+        lines += 1
+        words += len(line.split())
+print lines, words  
+# Output: 3, 12
+
