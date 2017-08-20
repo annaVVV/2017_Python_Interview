fd3e75f68bdedaccccbc5d9849cb5a96cc93157e
annaVVV <vergeles.anna@gmail.com>
2017-08-20 12:23:02 -0700

    TODO: need to add vertical lines to children

--- Interview_codingQQ/Vindicia/display_processes_hierarchy.py
+++ Interview_codingQQ/Vindicia/display_processes_hierarchy.py
@@ -1,40 +1,5 @@
 import sys, os
 
-def print_tree(current_node, indent="", last='updown'):
-
-    nb_children = lambda node: sum(nb_children(child) for child in node.children) + 1
-    size_branch = {child: nb_children(child) for child in current_node.children}
-
-    """ Creation of balanced lists for "up" branch and "down" branch. """
-    up = sorted(current_node.children, key=lambda node: nb_children(node))
-    down = []
-    while up and sum(size_branch[node] for node in down) < sum(size_branch[node] for node in up):
-        down.append(up.pop())
-
-    """ Printing of "up" branch. """
-    for child in up:
-        next_last = 'up' if up.index(child) is 0 else ''
-        next_indent = '{0}{1}{2}'.format(indent, ' ' if 'up' in last else '|', " " * len(current_node.name))
-        print_tree(child, indent=next_indent, last=next_last)
-
-    """ Printing of current node. """
-    if last == 'up': start_shape = '-'
-    elif last == 'down': start_shape = '_'
-    elif last == 'updown': start_shape = ' '
-    else: start_shape = '|'
-
-    if up: end_shape = '|'
-    elif down: end_shape = '-'
-    else: end_shape = ''
-
-    print '{0}{1}{2}{3}'.format(indent, start_shape, current_node.name, end_shape)
-
-    """ Printing of "down" branch. """
-    for child in down:
-        next_last = 'down' if down.index(child) is len(down) - 1 else ''
-        next_indent = '{0}{1}{2}'.format(indent, ' ' if 'down' in last else '|', " " * len(current_node.name))
-        print_tree(child, indent=next_indent, last=next_last)
-
 class Node:
     def __init__(self, name, pid ,parent=None):
         self.name = name
@@ -65,12 +30,12 @@ def walk(node, pref):
     if count_chilren==0:
         return
 
-    #if count_chilren > 1:
-
     for c in node.children:
         print "{: >5} ".format(c.pid), pref + '\_', c.name
         walk(c, '   ' + pref)
 
+        
+# Main        
 # read data file
 file = open('input', 'r')
 
