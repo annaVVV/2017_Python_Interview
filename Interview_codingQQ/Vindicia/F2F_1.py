8ed7818151ef3b6c51a87c632d94c1a438cd78b4
annaVVV <vergeles.anna@gmail.com>
2017-08-30 08:49:16 -0700

    Update F2F_1.py

--- Interview_codingQQ/Vindicia/F2F_1.py
+++ Interview_codingQQ/Vindicia/F2F_1.py
@@ -1,6 +1,16 @@
-# Implement method to verify if IP4 address is valid
-
+"""
+Data driven Test Cases using the unittest.TestSuite class, 
+which will allow you to dynamically construct a set of unittest.TestCase instances 
+which will get run separately. 
+The unittest.TestCase subclass should define just one test method, 
+with the class accepting a construction parameter passing in the value 
+to test against for that particular instance
+"""
 import unittest, re
+
+# Implement method to verify if IP4 address is valid:
+# valid values: 0-255.1-255.1-255.1-255
+# Leading 0 >> False (0), example: 123.01.123.123
 
 def validate_ip4_address(address):
     #print 'add:',address
@@ -40,7 +50,55 @@ if __name__ == '__main__':
     validate_ip4_address('0.1.1.1')
 
 
-# Unix commands: sed, awk
-#  remove empty lines from file 
+# Unix commands: sed, awk to  remove empty lines from file 
+
+# sed -i '/^$/d' file.txt
+# The -i means it will edit the file in-place
+
+# sed '/^$/d' file.txt
+# ^$ is a regular expression matching only a blank line, a line start followed by a line end.
+# d is the sed command to delete a line. 
+"""
+https://stackoverflow.com/questions/14570489/how-to-remove-blank-lines-from-a-unix-file
+awk 'NF' filename
+awk 'NF > 0' filename
+sed -i '/^$/d' filename
+awk '!/^$/' filename
+awk '/./' filename
+The NF also removes lines containing only blanks or tabs, the regex /^$/ does not.
+
+Use grep to match any line that has nothing between the start anchor (^) and the end anchor ($):
+grep -v '^$' infile.txt > outfile.txt
+If you want to remove lines with only whitespace, you can still use grep. 
+Using Perl regular expressions in this example, but here are other ways:
+grep -P -v '^\s*$' infile.txt > outfile.txt
+or, without Perl regular expressions:
+
+grep -v '^[[:space:]]*$' infile.txt > outfile.txt
+"""
 
 # SQL find second max salary and employee ID
+"""
+Query 1.1 Find the Employee with the Highest Salary
+
+SELECT EmployeeId, Salary FROM
+(Select EmployeeId, Salary, ROW_NUMBER() OVER(Order by Salary Desc) as Salary_Order from Employee) DT
+WHERE DT. Salary_Order = 1 ;
+
+Second Highest salary and EmployeeId
+
+select EmployeeId, Salary FROM Employee 
+where Salary = (
+SELECT MAX(salary) FROM Employee WHERE Salary NOT IN ( SELECT Max(Salary) FROM Employee));
+
+SELECT Id, Salary FROM Employee e WHERE 2=(SELECT COUNT(DISTINCT Salary) FROM Employee p WHERE e.Salary<=p.Salary)
+MySQL:
+SELECT TOP 1 Salary FROM ( SELECT TOP 2 Salary FROM Employee ORDER BY Salary DESC) AS MyTable ORDER BY Salary ASC
+
+
+"""
+
+
+
+
+
