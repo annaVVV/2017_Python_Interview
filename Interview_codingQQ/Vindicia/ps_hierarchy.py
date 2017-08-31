fcc2be7c15d090529f06ba392a8d99a737400643
annaVVV <vergeles.anna@gmail.com>
2017-08-31 09:43:07 -0700

    Update ps_hierarchy.py

--- Interview_codingQQ/Vindicia/ps_hierarchy.py
+++ Interview_codingQQ/Vindicia/ps_hierarchy.py
@@ -1,3 +1,15 @@
+"""
+* Run from terminal:
+```
+$ python ps_hierarchy.py > output     
+```
+
+* Script **ps_hierarchy.py.py**  executs command *"ps -e l"* and saves output into file **"in_file"**
+* **nodes**: a dictionary with **PID** as **keys** and **value** as a **Node** instance created for each command (see *class* **Node**)
+* Method **walk(node, pref)** recursively prints all commands hierarchy 
+* Result file: **output**
+"""
+
 import subprocess
 
 class Node:
